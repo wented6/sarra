41129629a48dd8eddf055f926861807130444c6f
wented6 <44713416+wented6@users.noreply.github.com>
2019-05-13 02:20:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,34 @@
-﻿
+const Discord = require('discord.js');
+const meercy = new Discord.Client();
+
+const adminprefix = "S";
+const devs = ['538676483455975431'];
+meercy.on('message', message => {
+    console.log("oNNN");
+
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'pl')) {
+  meercy.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+meercy.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+meercy.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith('ls')) {
+   meercy.user.setActivity(argresult, {type:'Listening'});
+    message.channel.send(` تم تغير اللسننق ألى **${argresult} \** `)
+} else        
+if (message.content.startsWith('wt')) {
+   meercy.user.setActivity(argresult, {type:'Watching'});
+    message.channel.send(` تم تغير الواتشنق ألى **${argresult} \** `)
+}
+});
+ meercy.login(process.env.B);
